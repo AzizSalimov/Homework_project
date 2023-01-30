4c358713e49144097090cef1492685e139517034
AzizSalimov <122611579+AzizSalimov@users.noreply.github.com>
2023-01-30 15:23:54 +0500

    ....

---
+++ Homework/Homework/task.1.py
@@ -0,0 +1,37 @@
+# def itrator():
+#     for i in range(1, 20):
+#         for j in range(i):
+#             if i % 2 == 0:
+#                     print(f"-{i}")
+#             break
+#
+#         else:
+#             yield i
+#
+#
+#
+# count = itrator()
+#
+# for _ in range(20):
+#     print(next(count))
+#
+
+
+
+def generator__():
+    for i in range(1, 21):
+         if i%2==0:
+          yield f"-{i}"
+         else:
+            yield i
+count = iter(generator__())
+
+for _ in range(7):
+    print(next(count))
+# print(next(conunt))
+# print(next(conunt))
+# print(next(conunt))
+# print(next(conunt))
+# print(next(conunt))
+# print(next(conunt))
+# print(next(conunt))
