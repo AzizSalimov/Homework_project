29b05ba4d393970787ce7ea8b958f9c6689669da
AzizSalimov <122611579+AzizSalimov@users.noreply.github.com>
2023-02-02 10:08:29 +0500

    Homewok

--- Homework/Hoework.2/Homewok2.py
+++ Homework/Hoework.2/Homewok2.py
@@ -1,52 +1,169 @@
-from hotelinfo import Hotel
-import tkinter as tk
-from tkinter import messagebox, END, Label, Entry, StringVar, Radiobutton
+# from cProfile import label
+#
+# from ffff.Glass.Hoework2.Homewok1 import inputFiled
+# from hotelinfo import Hotel
+# import tkinter as tk
+# from tkinter import messagebox, END, Label, Entry, StringVar, Radiobutton, Button
+# from datetime import date
+#
+#
+# window = tk.Tk()
+# window.title('Hotel Registra')
+# window.geometry('750x700')
+#
+# hotel_info = []
+# def add():
+#     hotel_inf = Hotel(
+#         name.get(),
+#         full_name.get(),
+#         price.get(),
+#     )
+#     hotel_info.append(hotel_inf.get_attr(as_dict=True))
+#     messagebox.showinfo("HOTEL information has been added")
+#
+#
+#
+# def age():
+#     today = date.today()
+#     print(today)
+#     year = int(inputFiled.get())
+#     if year <= 0 or year >= 2023:
+#         label['text'] = 'Xato'
+#     else:
+#         jj = today.year - year
+#
+#         label['text'] = 'You ' + str(jj) + ' years old'
+#
+#
+# name_ = Label(window, text='Your name:')
+# name_.place(x=165, y=10)
+# name = Entry(window, width=30, borderwidth=3)
+# name.place(x=230, y=10)
+#
+#
+# full_ = Label(window, text='Your full_name:')
+# full_.place(x=133, y=35)
+# full_name = Entry(window, width=30, borderwidth=3)
+# full_name.place(x=230, y=35)
+#
+#
+# gender = StringVar()
+# gender_Ty = {'room_1': "Standart", 'room_2': 'Luxy', 'room_3': 'Biznes'}
+# gender_ = Label(window, text='Room types:', padx=20, pady=95)
+# gender_.grid(row=30, column=2)
+# room_1 = Radiobutton(
+#     window, text=gender_Ty.get('room_1'), value='room_1', variable=gender
+# )
+# room_1.place(x=110, y=125)
+#
+# room_2 = Radiobutton(
+#     window, text=gender_Ty.get('room_2'), value='room_2', variable=gender
+# )
+# room_2.place(x=200, y=125)
+#
+# room_3 = Radiobutton(
+#     window, text=gender_Ty.get('room_3'), value='room_3', variable=gender
+# )
+# room_3.place(x=280, y=125)
+#
+#
+#
+# rooms = StringVar()
+# rooms_ = {'Standart': '500$', 'Luxy': '1000$', 'Biznes': '2000$'}
+# age_ = Label(window, text='Age info:')
+# age_.place(x=250, y=36)
+# age__ = Entry(window, width=30, borderwidth=3)
+# age__.place(x=300, y=60)
+#
+# price = Label(window, text='Room price:', padx=20, pady=10)
+# price.grid(row=40, column=2)
+#
+#
+#
+#
+# # age___ = Button(window, text='Age', padx=20, pady=10, command=age)
+# # age___.place(x=360, y=340)
+#
+#
+#
+# if __name__ == '__main__':
+#     window.mainloop()
 
+
+import codecs
+import tkinter as tk
+import csv, os
+from tkinter import *
+from tkinter import messagebox, Label, Entry, StringVar, Radiobutton
+from ffff.Glass.Hoework2.Car import Car
+from tkcalendar import DateEntry
 
 window = tk.Tk()
-window.title('Hotel Registra')
-window.geometry('750x700')
+window.title("Hotel")
+window.geometry('700x700')
 
-hotel_info = []
+roomss = []
+
+
 def add():
-    hotel_inf = Hotel(
+    room = Car(
         name.get(),
-        full_name.get(),
-        price.get(),
+        full_name_.get(),
+        age.get(),
     )
-    hotel_info.append(hotel_inf.get_attr(as_dict=True))
-    messagebox.showinfo("HOTEL information has been added")
+    room.append(roomss.get_attr(as_dict=True))
+    messagebox.showinfo("Added machine data")
 
 
-
-def age():
-    today = date.today()
-    print(today)
-    year = int(inputFiled.get())
-    if year <= 0 or year >= 2023:
-        label['text'] = 'Xato'
-    else:
-        jj = today.year - year
-
-        label['text'] = 'You ' + str(jj) + ' years old'
+def save():
+    with open('hotel.csv', 'a', newline='\n') as f:
+        header = ["Name, Full name, Age, price"]
+        csv_writer = csv.DictWriter(f, header)
+        if os.path.getsize('hotel.csv') == 0:
+            csv_writer.writeheader()
+        csv_writer.writerows(roomss)
+        messagebox.showinfo('Hotel Information', 'Saved successfully')
 
 
+def clear():
+    name.delete(0, END)
+    full_name_.delete(0, END)
+    age.delete(0, END)
+
+
+def information():
+    file_name = "hotel.csv"
+    encoding = 'utf-8'
+
+    with codecs.open(file_name, "r", encoding) as fp:
+        reader = csv.reader(fp)
+        for i in reader:
+            print(i)
+
+
+# your name
 name_ = Label(window, text='Your name:')
 name_.place(x=165, y=10)
 name = Entry(window, width=30, borderwidth=3)
 name.place(x=230, y=10)
 
+# your full name
+full_ = Label(window, text='Your full name:')
+full_.place(x=165, y=35)
+full_name_ = Entry(window, width=30, borderwidth=3)
+full_name_.place(x=255, y=35)
+# # age
+#
+age_ = Label(window, text='Your age:')
+age_.place(x=165, y=60)
+age = Entry(window, width=30, borderwidth=3)
+age.place(x=220, y=60)
 
-full_ = Label(window, text='Your full_name:')
-full_.place(x=133, y=35)
-full_name = Entry(window, width=30, borderwidth=3)
-full_name.place(x=230, y=35)
-
-
+# # car name
 gender = StringVar()
-gender_Ty = {'room_1': "Standart", 'room_2': 'Luxy', 'room_3': 'Biznes'}
-gender_ = Label(window, text='Room types:', padx=20, pady=95)
-gender_.grid(row=30, column=2)
+gender_Ty = {"room_1": 'Standart', 'room_2': 'Luxy', 'room_3': 'Biznes'}
+gender_label = Label(window, text='Room types:', padx=20, pady=95)
+gender_label.grid(row=30, column=2)
 room_1 = Radiobutton(
     window, text=gender_Ty.get('room_1'), value='room_1', variable=gender
 )
@@ -60,10 +177,38 @@ room_2.place(x=200, y=125)
 room_3 = Radiobutton(
     window, text=gender_Ty.get('room_3'), value='room_3', variable=gender
 )
-room_3.place(x=280, y=125)
+room_3.place(x=260, y=125)
 
+dob_label = Label(window, text="How long to stay in the room: ", padx=1, pady=20)
+dob_label.grid(row=45, column=3)
+dob_entry = DateEntry(window)
+dob_entry.grid(row=45, column=60)
 
+# color
+color = Label(window, text='Room price:', padx=20, pady=10)
+color.place(x=40, y=270)
+
+button = Button(window, text='400$', bg='blue', fg='white')
+button.place(x=160, y=280)
+
+bu = Button(window, text='1000$', bg='black', fg='white')
+bu.place(x=210, y=280)
+
+buk = Button(window, text='2000$', bg='gray', fg='white')
+buk.place(x=260, y=280)
+
+# save
+save_ = Button(window, text='SAVE', padx=20, pady=10, command=save)
+save_.place(x=100, y=340)
+
+info = Button(window, text='info', padx=20, pady=10, command=information)
+info.place(x=200, y=340)
+
+clear_ = Button(window, text='clear', padx=20, pady=10, command=clear)
+clear_.place(x=300, y=340)
+
+quit_btn = Button(window, text='EXIT', padx=20, pady=10, command=window.quit)
+quit_btn.place(x=400, y=340)
 
 if __name__ == '__main__':
     window.mainloop()
-
